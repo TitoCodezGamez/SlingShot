29342954effab0fd1d81e7cbf07e34eac25372ff
TitoCodes <titosaid2008@gmail.com>
2020-12-24 18:51:51 -0800

    The constraint class

---
+++ chain.js
@@ -0,0 +1,25 @@
+class Chain{
+
+    constructor(bodyA,bodyB){
+        var opt = {
+            bodyA : bodyA,
+            bodyB : bodyB,
+            length : 10,
+            stiffness : 0.1
+        }
+
+        this.chain = Constraint.create(opt);
+        World.add(world,this.chain);
+
+    }
+
+    display(){
+
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+
+}
